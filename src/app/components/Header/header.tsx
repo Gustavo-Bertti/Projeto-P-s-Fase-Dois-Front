2217e2d3a5c27dea5a95c136c80bb014e586bf65
Gustavo-Bertti <bertinhobertti@hotmail.com>
2025-09-19 17:04:05 -0300

    Adicionando o Header

---
+++ src/app/components/Header/header.tsx
@@ -0,0 +1,18 @@
+import Link from "next/link";
+
+const Header = () => {
+    return (
+        <header className="w-full bg-indigo-700 text-white py-4 px-30 flex justify-between items-center">
+            <h1 className="text-2xl font-bold">Blog Escolar</h1>
+            <nav className="mt-2 md:mt-0 flex gap-4">
+                <Link href="/" className="mr-4 hover:underline">
+                    Home
+                </Link>
+                <Link href="/about" className="hover:underline">
+                    Gerenciar
+                </Link>
+            </nav>
+        </header>
+    );
+}
+export default Header;
