694dbd48f59de6a2bf99141c42caecf390c6d637
Gustavo Macedo da Silva <x522403@CloudProdamAzhotmail.onmicrosoft.com>
2025-10-07 14:06:38 -0300

    ajuste merge

--- src/libs/api/services/post.ts
+++ src/libs/api/services/post.ts
@@ -15,21 +15,13 @@ export const getAllPosts = async () => {
         throw error;
     }
 }
-<<<<<<< HEAD
 export const createPost = async (post: Postagem) => {
     try {
         const res = await client.post<Postagem>('/postagem', post);
-=======
-
-export const getPostBySearch = async (search: string) => {
-    try {
-        const res = await client.get<Postagem[]>(`/postagem/search?termo=${encodeURIComponent(search)}`);
->>>>>>> 23765d63ad016d8c7965403a53640e4afe5c01f8
         return res.data;
     } catch (error) {
         throw error;
     }
-<<<<<<< HEAD
 }
 export const updatePost = async (post: Postagem) => {
     try {
@@ -66,6 +58,11 @@ export const getPostsByUserId = async () => {
         throw error;
     }
 }
-=======
+export const getPostBySearch = async (search: string) => {
+    try {
+        const res = await client.get<Postagem[]>(`/postagem/search?termo=${encodeURIComponent(search)}`);
+        return res.data;
+    } catch (error) {
+        throw error;
+    }
 }
->>>>>>> 23765d63ad016d8c7965403a53640e4afe5c01f8
